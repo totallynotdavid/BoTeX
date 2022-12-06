511016867c5da01cc58fda6751f0385ef15d0638
David D <dadch1404@gmail.com>
2022-12-06 01:07:46 -0500

    feat: processing messages for command

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	// bytes.Buffer
+	"context"
+	"fmt" // prints stuff
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
+)
+
+type MyClient struct {
+	WAClient       *whatsmeow.Client
+	eventHandlerID uint32
+}
+
+func (mycli *MyClient) register() {
+	mycli.eventHandlerID = mycli.WAClient.AddEventHandler(mycli.eventHandler)
+}
+
+func (mycli *MyClient) eventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		newMessage := v.Message
+		msg := newMessage.GetConversation()
+		fmt.Println("Message from:", v.Info.Sender.User, "->", msg)
+		if msg == "" {
+			return
+		}
+
+		if strings.HasPrefix(msg, "!latex") {
+			// Use the strings.TrimPrefix function to remove the "!latex" prefix from the input string
+			latexCode := strings.TrimPrefix(msg, "!latex")
+			fmt.Println(latexCode)
+		}
+
+	}
+}
+
+func main() {
+	dbLog := waLog.Stdout("Database", "DEBUG", false)
+
+	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
+	container, err := sqlstore.New("sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+
+	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+
+	clientLog := waLog.Stdout("Client", "DEBUG", false)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+	// add the eventHandler
+	mycli := &MyClient{WAClient: client}
+	mycli.register()
+
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	client.Disconnect()
+}
